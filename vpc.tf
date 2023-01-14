8b916d43ba919dde02bf97f67f827f78b974ed89
Akhilesh Sarda <akhilesh.sarda@oncconvergence.com>
2023-01-14 19:09:48 +0530

    add two AZs

--- vpc.tf
+++ vpc.tf
@@ -1,14 +1,32 @@
 resource "aws_vpc" "first-vpc" {
-    cidr_block = "10.0.0.0/16"
+    cidr_block = "11.0.0.0/16"
     tags = {
       "Name" = "test"
     }
 }
 
-resource "aws_subnet" "first-sub1" {
+resource "aws_subnet" "pri-first-sub1" {
     vpc_id = aws_vpc.first-vpc.id
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "11.0.0.0/19"
+    availability_zone = "us-west-2a"
     tags = {
-      "Name" = "test-sub1"
+      "Name" = "pri-first-sub1"
+      # for k8s int loadbalancer
+      # "kubernetes.io/role/internal-elb" = "1"
+      # for owning the subnets may be
+      # "kubernetes.io/cluster/demo" = "owned"
+    }
+}
+
+resource "aws_subnet" "pri-first-sub2" {
+    vpc_id = aws_vpc.first-vpc.id
+    cidr_block = "11.0.32.0/19"
+    availability_zone = "us-west-2b"
+    tags = {
+      "Name" = "pri-first-sub2"
+      # for k8s int loadbalancer
+      # "kubernetes.io/role/internal-elb" = "1"
+      # for owning the subnets may be
+      # "kubernetes.io/cluster/demo" = "owned"
     }
 }
