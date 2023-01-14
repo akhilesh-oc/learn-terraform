b3089e836d6e3537a24ee687479fee94f469a724
root <Akhilesh Sarda>
2023-01-14 13:22:42 +0530

    test sample vpc

---
+++ vpc.tf
@@ -0,0 +1,19 @@
+
+provider "aws" {
+    region = "use-west-2"
+}
+
+resource "aws_vpc" "first-vpc" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+      "Name" = "test"
+    }
+}
+
+resource "aws_subnet" "first-sub1" {
+    vpc_id = aws_vpc.first-vpc.id
+    cidr_block = "10.0.1.0/24"
+    tags = {
+      "Name" = "test-sub1"
+    }
+}
